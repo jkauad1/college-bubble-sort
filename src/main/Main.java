c3a3d60fdd39ee83d69869cc1e7a8e9cf201a31f
Kauã <gamersnes69@gmail.com>
2024-08-21 18:29:45 -0300

    Changed the array variable name and created a new one.

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,10 +6,16 @@ import static classes.BubbleSortController.bubbleSort;
 
 public class Main {
     public static void main(String[] args) {
-        int[] lista = {7,5,1,8,3};
+        int[] list = {7,5,1,8,3};
 
-        bubbleSort(lista);
+        bubbleSort(list);
 
-        System.out.println(Arrays.toString(lista));
+        System.out.println(Arrays.toString(list));
+
+        int[] list2 = {55, 2, 6, 2, 10, 11, 9};
+
+        bubbleSort(list2);
+
+        System.out.println(Arrays.toString(list2));
     }
 }
